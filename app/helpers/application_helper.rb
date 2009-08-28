0f78741a509b76c87a662df2f615e3e99bea4f65
Nicholas 'OwlManAtt' Evans <owlmanatt@gmail.com>
2009-08-28 16:16:56 +0000

    Number => ISK helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def number_to_isk(amount)
+    number_with_delimiter(amount.round(2)).to_s << ' isk'
+  end
 end
