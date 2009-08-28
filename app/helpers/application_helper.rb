06d92d1591b9c6d9927fbce2fe8014f8e0a59593
Nicholas 'OwlManAtt' Evans <owlmanatt@gmail.com>
2009-08-28 18:07:50 +0000

    Added a helper to ... spice up text.
    Ported from the Starchan Smarty modifier fagify <http://github.com/OwlManAtt/smarty-ys/blob/d751fa657fdce18f7a8f551bae0caae7bc97fa14/plugins/modifier.fagify.php>.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,43 @@ module ApplicationHelper
   def number_to_isk(amount)
     number_with_delimiter(amount.round(2)).to_s << ' isk'
   end
+
+  def fagify_text(source_text)
+    colours = ['00FFFF','FF7F50','7FFF00','DC143C','8B008B',
+      '8FBC8F','CD5C5C','7CFC00','FA8072','2E8B57',
+      '9ACD32','CCFFFF','99FFCC','FFFF33','99CC00',
+      'FF99CC','CC99FF','9933CC','FF3366','990066',
+      '66FF33','0099CC','669999','0033FF','003366',
+      '666600','0000CC','FF3300','336699','006666',]
+    sizes = ['10pt', '15pt', '17pt', '17pt', '18pt','22pt']
+    ignore = [' ',"\n"]
+
+    faggy_text = ''
+    source_text.each_char do |char|
+      unless ignore.include?(char)
+        fg = colours[rand(colours.size)]
+        bg = (fg.to_i(16) ^ 0xffffff).to_s(16).rjust(6,'0')
+        styles = ["color: ##{fg}", "background-color: ##{bg}"]
+        
+        if rand(1) == 0
+          size = sizes[rand(sizes.size)]
+          styles << "font-size: #{size}"
+        end
+
+        if rand(3) == 0
+          styles << 'text-decoration: blink'
+        end
+
+        if rand(5) == 0
+          styles << 'font-weight: bold'
+        end
+
+        char = "<span style='#{styles.join(';')}'>#{char}</span>"
+      end # unless
+
+      faggy_text << char
+    end # each_char
+
+    return faggy_text
+  end # fagify_text
 end
